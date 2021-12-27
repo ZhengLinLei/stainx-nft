8719f7286a448bfa762bb8ce512bd0db5bce2ad3
ZLL <zheng9112003@gmail.com>
2021-12-27 15:05:30 +0100

    Mobile drag version

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var moveCard;
 window.addEventListener('load', ()=>{
     // Card move
-    let cardArr = document.querySelectorAll('.card');
+    // let cardArr = document.querySelectorAll('.card');
+    let cardWindow = document.querySelector('.card-window');
     let cardList = document.querySelector('.card-window > div');
     let pointArr = document.querySelectorAll('.card-img .point');
     let cardFocusPos = 0;
@@ -28,6 +29,25 @@ window.addEventListener('load', ()=>{
         moveCard(i);
     }, 100000);
 
+    // MOBILE DRAG AND MOVE
+    let xPos = 0;
+    let cardWidth = cardWindow.offsetWidth;
+
+    cardWindow.addEventListener('touchstart', e =>{
+        xPos = e.changedTouches[0].pageX;
+    });
+    cardWindow.addEventListener('touchend', e =>{
+        let x = e.changedTouches[0].pageX - xPos;
+        if(Math.abs(x) >= cardWidth/3){
+            if(x < 0){
+                let i = (cardFocusPos == 3)?0:cardFocusPos+1;
+                moveCard(i);
+            }else{
+                let i = (cardFocusPos == 0)?3:cardFocusPos-1;
+                moveCard(i);
+            }
+        }
+    });
 
     // ASSETS
     setInterval(()=>{
